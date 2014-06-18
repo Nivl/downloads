1e36f195c15bbe023f58d952e5c4e56131350b4d
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-06-17 17:25:06 -0700

    Add capitalize filter

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,11 @@
 
-angular.module('app', ['app-controllers', 'app-directives']);
+angular.module('app', ['app-controllers', 'app-directives', 'app-filters', 'ui.bootstrap']);
 angular.module('app-controllers', []);
 angular.module('app-directives', []);
+angular.module('app-filters', []);
 
 require('scripts/controllers/*');
+require('scripts/filters/*');
 require('scripts/directives/*');
 
 require('scripts/custom');
---
+++ app/scripts/filters/capitalize.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('app-filters').filter('capitalize', function () {
+  return function (input) {
+    input = input || '';
+
+    var out = input.charAt(0).toUpperCase() + input.slice(1);
+    var find = ['A ', ' An ', ' The ', ' At ', ' By ', ' For ', ' In ', ' Of ', ' On ', ' To ',
+      ' Up ', ' And ', ' As ', ' But ', ' It ', ' Or ', ' Nor '];
+    var replace = [' a ', ' an ', ' the ', ' at ', ' by ', ' for ', ' in ', ' of ', ' on ',
+      ' to ', ' up ', ' and ', ' as ', ' but ', ' it ', ' or ', ' nor '];
+
+    var regex;
+    for (var i = 0; i < find.length; i += 1) {
+      regex = new RegExp(find[i], 'g');
+      out = out.replace(regex, replace[i]);
+    }
+
+    return out;
+  };
+});
