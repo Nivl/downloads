9d90bb5ff68ec1445a306fb7030298c739372584
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-07-05 22:43:29 -0700

    Add htaccess

--- web.js
+++ web.js
@@ -2,12 +2,19 @@
 
 var express = require('express');
 var compression = require('compression');
+var auth = require('http-auth');
+
+var basic = auth.basic({
+  realm: 'Restricted Area.',
+  file: __dirname + '/.htpasswd'
+});
+
 
 var app = express();
 var thirtyMinutes = 1800000;
 
 app.use(compression());
-
+app.use(auth.connect(basic));
 app.use(express.static(__dirname + '/dist'), { maxAge: thirtyMinutes });
 
 app.get('/', function (req, res) {
